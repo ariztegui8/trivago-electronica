ab3b496b588ef19d8704bd3ea9534425dfc52602
ariztegui8 <jorgeariztegui8@gmail.com>
2024-06-25 11:19:49 -0300

    red

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,21 +3,13 @@ import Drawer from './Drawer'
 import { CarrouselProvider } from './context/CarrouselProvider';
 import { ProductProvider } from './context/ProductProvider'
 import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Trivago Electronica',
-  description: 'Web site Trivago Electronica',
-}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <CarrouselProvider>
         <ProductProvider>
-          <body className={inter.className}>
+          <body>
             <Drawer />
             {children}
           </body>
